4d0194af7cd8375f2798236c424ffa9452ef8ebe
romanp <roman_ck@yahoo.com>
2018-02-04 10:20:06 +0100

    erster commit

---
+++ src/main/java/romans/MyMain.java
@@ -0,0 +1,25 @@
+package romans;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(MyMain.class);
+
+		int counter = 0;
+		
+		do {
+			logger.info("XXXXXX: "+counter);
+			counter++;
+			try {
+				Thread.sleep(1000L);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} while (counter < 20);
+
+	}
+
+}
