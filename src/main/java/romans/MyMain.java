4ba22d8d8143b7d7a38e5d24c7fe1473f6c2f251
romanp <roman_ck@yahoo.com>
2018-02-10 17:54:43 +0100

    alles kommt rein

--- src/main/java/romans/MyMain.java
+++ src/main/java/romans/MyMain.java
@@ -11,7 +11,7 @@ public class MyMain {
 		int counter = 0;
 		
 		do {
-			logger.info("XXXXXX: "+counter);
+			logger.info("XXXXXXX YYY: "+counter);
 			counter++;
 			try {
 				Thread.sleep(1000L);
